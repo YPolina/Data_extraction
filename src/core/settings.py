06230e880493ec4ba96363a83ae6dfe3a449e9ac
Palina <palina.yatsko@innowise.com>
2025-10-07 13:30:28 +0200

    Set url to correct psycopg version

--- src/core/settings.py
+++ src/core/settings.py
@@ -30,7 +30,7 @@ DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
-DATABASE_URL_psycopg = f"postgresql+psycopg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL_psycopg = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 EMAIL = os.getenv("EMAIL_ADDRESS")
